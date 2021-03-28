c456d10bc583842b1b593c63a5f41daeb23007e7
RyanChai-1901830 <70925767+RyanChai-1901830@users.noreply.github.com>
2021-03-28 13:39:44 +0800

    Added power saving codes & documentation including mini experiment

---
+++ Power saving/LowPowerMode/LowPowerIncludingInterrupt/LowPowerIncludingInterrupt.ino
@@ -0,0 +1,37 @@
+#include "LowPower.h"
+//#define BUTTON_PIN 3
+//code above, example to define your pin to recieve interrupt from
+
+void setup() {
+  
+  //Set up default build in led
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  //pinMode(BUTTON_PIN, INPUT);
+  //Code above, example to setup your interrupt pin as an input pin
+
+  //attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), blinkLed, RISING);
+  //Code above, 1st parameter = pin interrupt, 2nd parameter = function, 3rd parameter = types of interrupt
+}
+
+void loop() {
+
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);
+
+  //Change the duration of the sleep mode by adjusting the first parameter
+  //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF); //Normal sleep mode
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); //Deep sleep mode
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(8000);                       // wait for 8 second
+
+}
+
+//Power saving mini experiments
+//On normal case, when led is lit, consume average of 0.223W to 0.238W & 110.2ohm to 110.5ohm
+
+//LowPower.powerDown (save the most power)
+//When device put to power down, consume average of 0.144W & 171.2ohm to 184.1ohm
+
+//LowPower.idle
+//When device put to idle, consume average of 0.173W to  & 141.8 ohm to 141.9 ohm
